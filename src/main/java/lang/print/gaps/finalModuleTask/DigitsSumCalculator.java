92f42385eae07cb52042186ca75cd3d577be5f20
LVLAD2009@yandex.ru <24233lebedko>
2023-04-10 22:37:46 +0300

    stage0-module3-final

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,14 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int res1 = number % 10;
+        number = number / 10;
+        int res2 = number % 10;
+        number = number / 10;
+        int res3 = number % 10;
+        number = number / 10;
+        int res4 = number % 10;
+        System.out.println(res1 + res2 + res3 + res4);
+
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,18 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int revert = 0;
+        int lastNum1 = number % 10;
+        int result = revert * 10 + lastNum1;
+        int number2 = number / 10;
+
+        int lastNum2 = number2 % 10;
+        result = result * 10 + lastNum2;
+        int number3 = number2 / 10;
+
+        int lastNum3 = number3 % 10;
+        result = result * 10 + lastNum3;
+        System.out.println(result);
+
     }
 }
